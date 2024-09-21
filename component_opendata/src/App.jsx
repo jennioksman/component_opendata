599b8750c7e7b43f8e5c99fabca1234bcbcaacb9
jennioksman <c3okje00@students.oamk.fi>
2024-09-21 19:02:42 +0300

    Header-component made

--- component_opendata/src/App.jsx
+++ component_opendata/src/App.jsx
@@ -2,13 +2,20 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return(
-    <div>
-      
-    </div>
-  )
+  return( //Koodia on kätevä toistaa, koska voi käyttää samaa komponenttia toistamatta koodia
+    <>
+       <Header/>
+    </>   
+)
 }
 
+function Header(){
+return( //Voi palauttaa vain yhden elementin eli h1 ja p on laitettava "divin" sisään
+    <div>
+        <img src="./src/assets/MBMIlogo.png" alt="logo" />
+        <h1>Welcome to product page!</h1>
+    </div>
+)
+}
 export default App
