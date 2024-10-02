7f0eb4218b5980ea20139377e4fb6d296a03768b
jennioksman <c3okje00@students.oamk.fi>
2024-10-02 18:34:48 +0300

    A list of products and selection of them made.

--- component_opendata/src/App.jsx
+++ component_opendata/src/App.jsx
@@ -3,66 +3,88 @@ import './App.css'
 
 function App() {
 
-  return( //Koodia on kätevä toistaa, koska voi käyttää samaa komponenttia toistamatta koodia
+  return ( //Koodia on kätevä toistaa, koska voi käyttää samaa komponenttia toistamatta koodia
     <>
-       <Header/>
-       <ProductForm/>
-       <OrderInfo/>
-    </>   
-)
+      <Header />
+      <ProductForm />
+      <OrderInfo />
+    </>
+  )
 }
 
-function Header(){
-return( //Voi palauttaa vain yhden elementin eli h1 ja p on laitettava "divin" sisään
+function Header() {
+  return ( //Voi palauttaa vain yhden elementin eli h1 ja p on laitettava "divin" sisään
     <div className='header'>
       <div><img src="./src/assets/MBMIlogo.png" alt="logo" /></div>
       <div><h1>Welcome to product page!</h1></div>
     </div>
-)
-}
-
-function ProductForm(){
-
-    const [product, setProduct] = useState("choose");
-    const [st, setSt] = useState(0)
-  
-    const handleChange = (event) => {
-      setProduct(event.target.value)
-    }
-  
-    return (
-      <div>
-        <h3>Select product</h3>
-        <div className='product'>
-          <p>Product: </p>
-          <form>
-            <select value={product} onChange={handleChange}>
-              <option value="choose">Choose option</option>
-              <option value="hook">Hook 3mm</option>
-              <option value="needels">Needles 6mm</option>
-              <option value="yarn">Drops Merino Exrta Fine Beige</option>
-            </select>
-          </form>
-        </div>
-        <div className='quantity'>
-          <p>Quantity: </p>
-          <button onClick={()=> setSt(prev => prev > 0 ? prev - 1 : 0)}>-</button>
-          <p>{st}</p>
-          <button onClick={()=> setSt(prev => prev+1)}>+</button>
-        </div>
-      </div>
   )
 }
 
-  function OrderInfo(){
-    return( //Voi palauttaa vain yhden elementin eli h1 ja p on laitettava "divin" sisään
-        <div>
-          <h3>Order Info</h3>
-          <div className='array'>
-            <h5>Here is going to be array</h5>
-          </div>
-        </div>
-    )
-    }
+function ProductForm() {
+
+  const [product, setProduct] = useState("choose")
+  const [price, setPrice] = useState(null)
+  const [st, setSt] = useState(0)
+
+  const products = [
+    { name: "Wooden Hook 3mm", price: 7.90 },
+    { name: "Wooden Needles 4mm", price: 10.80 },
+    { name: "Cotton-merino 50g", price: 4.90 },
+    { name: "Kid-Silk 50g", price: 6.90 },
+    { name: "Merino Extra Fine 50g", price: 4.90 }
+  ]
+
+  const handleChange = (event) => {
+    const selectedProduct = products.find(p => p.name === event.target.value);
+    setProduct(selectedProduct.name);
+    setPrice(selectedProduct.price);
+    console.log("Selected Product Price:", selectedProduct.price);
+  }
+
+  
+
+  return (
+    <div>
+      <h3>Select product</h3>
+      <div className='product'>
+        <p>Product: </p>
+        <select value={product} onChange={handleChange}>
+          {
+            products.map(p => <option key={p.name} value={p.name}>{p.name} ({p.price})€</option>)
+          }
+        </select>
+      </div>
+      <div className='quantity'>
+        <p>Quantity: </p>
+        <button onClick={() => setSt(prev => prev > 0 ? prev - 1 : 0)}>-</button>
+        <p>{st}</p>
+        <button onClick={() => setSt(prev => prev + 1)}>+</button>
+      </div>
+    </div>
+  )
+}
+
+function OrderInfo() {
+  return ( //Voi palauttaa vain yhden elementin eli h1 ja p on laitettava "divin" sisään
+    <div>
+      <h3>Order Info</h3>
+      <div className='array'>
+        <table>
+          <tr>
+            <th>Product</th>
+            <th>Quantity</th>
+            <th>Total</th>
+          </tr>
+          <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  )
+}
 
 export default App
