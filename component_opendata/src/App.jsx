8e91c6b1a9e312c04ae304d41d84e4de371c761a
jennioksman <c3okje00@students.oamk.fi>
2024-10-15 20:51:21 +0300

    No-needed components removed from app.jsx

--- component_opendata/src/App.jsx
+++ component_opendata/src/App.jsx
@@ -1,12 +1,9 @@
 import { FindDrink, Header2, OpenData } from './components/coctailcorner'
-import { Header, ProductForm } from './components/productpage'
 
 function App() {
 
   return ( 
     <>
-      <Header/>
-      <ProductForm/>
       <Header2/>
       <div className='component-container'>
         <OpenData/>
